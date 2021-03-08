7f289fe0c58f22cef568ddb8268ca23800a80bf2
xiaozhou <993749927@qq.com>
2021-03-08 22:02:02 +0800

    第一个发布版本

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,62 @@
 <template>
   <div id="app">
-    <yanziPlayer :initVolume="initVolume" :isShowMusic.sync="isShowMusic" :prePath="prePath" :musicList.sync="musicList"
-      :zIndex="zIndex" @uploadLRC="uploadLRC" @uploadMusic="uploadMusic"></yanziPlayer>
+    <yanziPlayer :initVolume="initVolume" :isShowMusic.sync="isShowMusic" :prePath="prePath" :musicList.sync="musicList" :zIndex="zIndex" @uploadLRC="uploadLRC" @uploadMusic="uploadMusic" @uploadMusicIcon="uploadMusicIcon"></yanziPlayer>
   </div>
 </template>
 
 <script>
-  import yanziPlayer from "./components/yanzi-player.vue";
+import yanziPlayer from "./components/yanzi_player.vue";
 
-  export default {
-    name: "App",
-    components: {
-      yanziPlayer
+export default {
+  name: "App",
+  components: {
+    yanziPlayer
+  },
+  data() {
+    return {
+      zIndex: "1111",
+      musicList: [
+        // {
+        //   author: "CG",
+        //   image: "",
+        //   lrc: "",
+        //   name: "文爱",
+        //   src: "http://192.168.1.238:9995/music/CG - 文爱.mp3"
+        // },
+        // {
+        //   author: "爱乐团王超",
+        //   image: "",
+        //   lrc: "",
+        //   name: "死而无憾",
+        //   src: "http://192.168.1.238:9995/music/爱乐团王超 - 死而无憾.mp3"
+        // },
+      ],
+      prePath: "",
+      isShowMusic: false,
+      initVolume: 50
+    };
+  },
+  methods: {
+    uploadLRC(music) {
+      console.log("uploadLRC", music);
     },
-    data() {
-      return {
-        zIndex: "1111",
-        musicList: [
-          // {
-          //   author: "CG",
-          //   image: "",
-          //   lrc: "",
-          //   name: "文爱",
-          //   src: "http://192.168.1.238:9995/music/CG - 文爱.mp3"
-          // },
-          // {
-          //   author: "爱乐团王超",
-          //   image: "",
-          //   lrc: "",
-          //   name: "死而无憾",
-          //   src: "http://192.168.1.238:9995/music/爱乐团王超 - 死而无憾.mp3"
-          // },
-        ],
-        prePath: "",
-        isShowMusic: false,
-        initVolume: 50
-      };
+    uploadMusic(music) {
+      console.log(music);
     },
-    methods: {
-      uploadLRC(lrc) {
-        console.log("uploadLRC", lrc)
-      },
-      uploadMusic(file) {
-        console.log(file)
-      }
+    uploadMusicIcon(music) {
+      console.log(music);
     }
-  };
+  }
+};
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
